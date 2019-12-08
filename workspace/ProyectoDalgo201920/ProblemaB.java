f032d4767f3447e81b6cd6c212b0f1cafec94374
Cohnan <sc.puerto10@uniandes.edu.co>
2019-12-07 23:13:56 -0500

    Estuve pensando todo el B con un algoritmo incorrecto...

--- workspace/ProyectoDalgo201920/ProblemaB.java
+++ workspace/ProyectoDalgo201920/ProblemaB.java
@@ -45,6 +45,22 @@ public class ProblemaB
         	isMinLex = new Hashtable<Integer, Integer> ();
         	isFirstL = new Hashtable<Integer, Integer> ();
         	
+        	for (Persona persAct : lista) {
+        		int t = candidatos.size();
+        			
+        			// Si el elemento actual de b es menor a cualquier otra cabeza de los candidatos
+        		if (candidatos.size() == 0 || persAct.b < candidatos.get(t-1).get(0).b ) {
+        				// Agregar nueva lista candidata
+        			candidatos.add( new ArrayList<Persona>() );
+        			candidatos.get(0).add(persAct);
+        			
+        				// Actualizar estructuras
+        			update(persAct, 1, t);
+        		}
+        		
+        		int i = busquedaBinariaCols(persAct.b);
+        		
+        	}
         	
         		// Restart
         	N = Integer.parseInt(reader.readLine());
@@ -52,7 +68,27 @@ public class ProblemaB
         
     }
     
-    private static int iMinLex(int l) {
+    private static int busquedaBinariaCols(int b) {
+    	int lo = 0, hi = candidatos.size() - 1; 
+        while (lo <= hi) { 
+            int m = lo + (hi - lo) / 2; //
+            int elemAct = candidatos.get(m).get(candidatos.get(m).size() - 1).b; 
+            if (elemAct == b) 
+                return m+1; // Maximo hay uno que es igual al que busco
+  
+            if (elemAct < b)  
+                hi = m - 1;	// Por lo que las colas estan ordenadas desccendentemente
+  
+            else
+                lo = m + 1; 
+        } 
+  
+        // if we reach here, then element was 
+        // not present 
+        return lo;
+	}
+
+	private static int iMinLex(int l) {
     	if (isMinLex.containsKey(l)) {
     		return candidatos.size(); 
     	}
@@ -64,6 +100,17 @@ public class ProblemaB
     		return candidatos.size(); 
     	}
     	return isFirstL.get(l);
+    }
+    
+    private static void update(Persona persAct, int l, int t) {
+    	if (iFirstL(l) == t+1) {
+			isFirstL.put(l, t);
+			isMinLex.put(l, t);
+		} else {
+			isMinLex.put(l, 
+					persAct.index < isMinLex.get(l)? persAct.index : isMinLex.get(l) 
+							);  
+		}
     }
     
     public static class Persona {
@@ -94,14 +141,14 @@ public class ProblemaB
     public static class ComparatorA implements Comparator<Persona> {
         @Override
         public int compare(Persona p1, Persona p2) {
-            return -(p1.a - p2.a);
+            return (p1.a - p2.a);
         }
     }
     
     public static class ComparatorB implements Comparator<Persona> {
         @Override
         public int compare(Persona p1, Persona p2) {
-            return (p1.b - p2.b);
+            return -(p1.b - p2.b);
         }
     }
 }
